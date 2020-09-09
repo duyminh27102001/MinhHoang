716217e250219e7dcbb88aaa54fc71bb31ee39ee
unknown <minhhdth1907034@fpt.edu.vn>
2020-09-09 11:17:53 +0700

    add

---
+++ Email.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1907A.EmailPages
+{
+    class Email
+    {
+        public string Name { get; set; }
+        public string TieuDe { get; set; }
+        public string Content { get; set; }
+
+        public Email(string name, string tieude, string content)
+        {
+            this.Name = name;
+            this.TieuDe = tieude;
+            this.Content = content;
+        }
+        public override string ToString()
+        {
+            return Name + "--" + TieuDe + "--" + Content;
+        }
+    }
+}
